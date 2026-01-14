21a603f8c45d6caeb409321daf312a2647ce9889
justasstulpinas <sjustas1999@gmail.com>
2026-01-14 11:55:27 +0200

    day 3 created http request session, and register for user

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from typing import Generator
 DATABASE_URL = "sqlite:///./pacta.db" 
 
 engine = create_engine(
@@ -15,4 +15,15 @@ SessionLocal = sessionmaker(
 
 Base =declarative_base()
 
-# day2 sukurta duombaze
+# day2 sukurta duombaze pacta/app/pacta.db
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# day 3 per get_db() FastAPI kiekvienam http requestui sukuria atskirą sqlalchemy sesija, kuri naudojama DB operacijoms atlikti,
+# pasibaigus requestui sesija automatiskai uzdaroma, todel kiekvienas vartotjas veiksmus atlieka tik savo sesijoje.
+#  kiekvienas useris gali requesta daryti kiek nori kartu nes visada sukuriamas naujas http requestas.
